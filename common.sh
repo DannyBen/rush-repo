9e34f6d5fd7cfe0a00b40e1b5affba2dc0961e8b
Danny Ben Shitrit <db@dannyben.com>
2023-07-25 11:48:37 +0000

    add some support for Arch Linux

--- common.sh
+++ common.sh
@@ -68,7 +68,6 @@ general_install_helper() {
   fi
 }
 
-
 github_install_helper() {
   binary_name="$1"
   github_repo="$2"
@@ -146,3 +145,22 @@ temp_dir() {
 pushtmp() {
   pushd "$(temp_dir)"
 }
+
+# Return the linux distro as a lowercase string
+linux_distro() {
+  if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    echo "$ID" | tr '[:upper:]' '[:lower:]'
+  elif [ -f "/etc/lsb-release" ]; then
+    source /etc/lsb-release
+    echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]'
+  elif [ -f "/etc/debian_version" ]; then
+    echo "debian"
+  elif [ -f "/etc/redhat-release" ]; then
+    echo "centos"
+  else
+    echo "unknown"
+  fi
+}
+
+DISTRO="$(linux_distro)"
