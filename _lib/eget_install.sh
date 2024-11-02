a132f72f1bbca03a788a02410042a9d685fdadea
Danny Ben Shitrit <db@dannyben.com>
2024-11-02 04:55:03 +0000

    automatic commit

--- _lib/eget_install.sh
+++ _lib/eget_install.sh
@@ -1,6 +1,7 @@
 eget_install() {
   package="$1"
+  shift
   say "installing $package"
   command_exist eget || needs eget
-  sudo eget --upgrade-only --to=/usr/local/bin "$package"
+  sudo eget --upgrade-only --to=/usr/local/bin "$package" "$@"
 }
