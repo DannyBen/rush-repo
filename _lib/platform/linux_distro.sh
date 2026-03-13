4fd65cbdcc05ffaf1bb0fd756367e06438b6ed1b
Danny Ben Shitrit <db@dannyben.com>
2026-03-13 11:30:38 +0200

    update distro detection

--- _lib/platform/linux_distro.sh
+++ _lib/platform/linux_distro.sh
@@ -2,15 +2,15 @@
 linux_distro() {
   if [ -f "/etc/os-release" ]; then
     source /etc/os-release
-    echo "$ID" | tr '[:upper:]' '[:lower:]'
+    echo "${ID:-unknown}" | tr '[:upper:]' '[:lower:]'
   elif [ -f "/etc/lsb-release" ]; then
     source /etc/lsb-release
-    echo "$DISTRIB_ID" | tr '[:upper:]' '[:lower:]'
+    echo "${DISTRIB_ID:-unknown}" | tr '[:upper:]' '[:lower:]'
   elif [ -f "/etc/debian_version" ]; then
     echo "debian"
   elif [ -f "/etc/redhat-release" ]; then
     echo "centos"
-  elif [ "$(uname)" == "Darwin" ]; then
+  elif [ "$(uname)" = "Darwin" ]; then
     echo "mac"
   else
     echo "unknown"
